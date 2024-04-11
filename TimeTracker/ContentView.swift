dfdae101e0bacd5118a8919357a0e2081ad46396
Serena Wollersheim <sewoller@hotmail.com>
2024-04-11 09:00:16 -0700

    Created first timer layout. All UI changes, no backend. Added "todo" list.

--- TimeTracker/ContentView.swift
+++ TimeTracker/ContentView.swift
@@ -26,18 +26,82 @@ struct ContentView: View {
 //                }
                 RoundedRectangle(cornerRadius: 10)
                                 .fill(lightPink)
-                                .frame(width: screenSize.width, height: screenSize.height * 0.17)
+                                .frame(width: screenSize.width, height: screenSize.height * 0.18)
                                 .position(x: screenSize.width/2, y: 0)
-                VStack{
+                VStack(spacing:2){
                     Text("TimeTracker")
                         .font(.custom("Georgia Bold", size: 30))
                         //.padding(.bottom, 200)
                     HStack{
-                        Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
-                            Text("🏠")
-                        })
+                        //home page button
+                        Button(action: {}){
+                            Image(systemName: "house") .padding(5)
+                        }.foregroundColor(.white)
+                            .background(.red)
+                            .cornerRadius(5)
+                        //list page button
+                        Button(action: {}){
+                            Image(systemName: "list.bullet") .padding(5)
+                        }.foregroundColor(.white)
+                            .background(.red)
+                            .cornerRadius(5)
+                        //stats page button
+                        Button(action: {}){
+                            Image(systemName: "clock") .padding(5)
+                        }.foregroundColor(.white)
+                            .background(.red)
+                            .cornerRadius(5)
+                        //about page button
+                        Button(action: {}){
+                            Image(systemName: "questionmark.app") .padding(5)
+                        }.foregroundColor(.white)
+                            .background(.red)
+                            .cornerRadius(5)
                     }
-                    Spacer()
+                    
+                    let itemView = UIView()
+                    
+                    VStack{ // TIMER 1             f c
+                        HStack{ // name and buttons
+                            Text("App Development").font(.custom("Georgia", size: 28)).padding([.leading], 5)
+                            Spacer() // keeps the title and the buttons on the sides
+                            Button(action: {}){//start/stop button
+                                Image(systemName:"play")
+                                    .resizable() // allows resizing
+                                    .aspectRatio(contentMode: .fit) // stops image from stretching
+                                    .frame(width: 23, height: 23) // fits image within a frame
+                            }.foregroundColor(.black).padding(5) //end play button
+                            Button(action: {}){//edit button
+                                Image(systemName:"pencil")
+                                    .resizable() // allows resizing
+                                    .aspectRatio(contentMode: .fit) // stops image from stretching
+                                    .frame(width: 23, height: 23) // fits image within a frame
+                            }.foregroundColor(.black).padding(5) // end edit button
+                        } // end name and buttons
+                        HStack{ //timer section/type
+                            Text("Programming").font(.custom("Georgia", size: 15))
+                                .background(RoundedRectangle(cornerRadius: 4)
+                                    .fill(.orange)
+                                    .padding(-3)) // this padding makes sure the background fully encompasses the text
+                                .padding([.leading], 30) // this offsets the text from the left edge
+                            Spacer() // this keeps it to the left side, rather than centered
+                        } // end timer section/type
+                        HStack{ //timer time
+                            Text("5 hours, 45 minutes").font(.custom("Georgia Bold", size: 18))
+                                .padding([.leading], 30) // this offsets the text from the left edge
+                            Spacer() // this keeps it to the left side, rather than centered
+                        } //.padding([.top], 1)// end timer time
+                        HStack{ //timer description
+                            Text("Time spend developing my timer application using Swift. The goal is to publish it on the app store. ").font(.custom("Georgia", size: 15))
+                                .padding([.leading], 30) // this offsets the text from the left edge
+                            Spacer() // this keeps it to the left side, rather than centered
+                        } //.padding([.top], 1)// end timer description
+                        
+                    }.padding(5).background().cornerRadius(10).padding(20) // padding and background for the timer object
+                    // end timer object
+                    
+                    
+                    Spacer() // Keeps everything at the top of the page
                 }
             }
             
